5c0e3121665932286a266abdc6a6d0ebb4e1ce91
Alexander Neo <AlexanderNeo427@gmail.com>
2025-02-26 21:27:51 +0800

    Added some different colouring/thickness for the different road types

--- src/scripts/scenes/ScenePathfinding.ts
+++ src/scripts/scenes/ScenePathfinding.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { Feature, FeatureCollection, LineString, Position } from 'geojson';
 import sgGeoData from '../../assets/sg_geodata.json'
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { Line2, LineGeometry, LineMaterial, OrbitControls } from 'three/examples/jsm/Addons.js';
 import SceneBase from './SceneBase';
 import { BoundingBox, SceneID } from '../Declarations';
 import proj4 from 'proj4';
@@ -17,10 +17,6 @@ export default class ScenePathfinding extends SceneBase {
         this._camera.far = 800
         this._camera.position.set(0, 0, 100)
         this._camera.updateProjectionMatrix()
-
-        window.addEventListener('resize', () => {
-            // Resizing the camera based on the canvas? 
-        })
         this._camControls = this._setupCamControls(this._camera, renderer)
     }
 
@@ -38,9 +34,8 @@ export default class ScenePathfinding extends SceneBase {
         )
         this._camera.left = botLeft[0]
         this._camera.right = topRight[0]
-
         const adjustCameraForAspectRatio = () => {
-            const aspectRatio = window.innerWidth / window.innerHeight;
+            const aspectRatio: number = window.innerWidth / window.innerHeight;
             const camViewWidth: number = Math.abs(topRight[0] - botLeft[0])
             const targetViewHeight: number = (camViewWidth / aspectRatio)
             const yCenter: number = Math.abs(topRight[1] - botLeft[1]) * 0.5 + botLeft[1]
@@ -57,14 +52,22 @@ export default class ScenePathfinding extends SceneBase {
                 return
             }
             const coordinates: Position[] = feature.geometry.coordinates
-            const points: THREE.Vector2[] = coordinates.map((coord: Position) => {
-                const projectedCoord = proj4('EPSG:4326', 'EPSG:3857', coord);
-                return new THREE.Vector2(projectedCoord[0], projectedCoord[1]);
+            const points = coordinates.flatMap((coord: Position) => {
+                const projectedCoords = proj4('EPSG:4326', 'EPSG:3857', coord);
+                return [projectedCoords[0], projectedCoords[1], 0]
             })
 
-            const lineGeometry = new THREE.BufferGeometry().setFromPoints(points)
-            const lineMaterial = new THREE.LineBasicMaterial({ color: 0xFFFFFF })
-            const line = new THREE.Line(lineGeometry, lineMaterial)
+            const highwayType: string = feature.properties?.highway || ""
+
+            const line = new Line2(
+                new LineGeometry().setPositions(points),
+                new LineMaterial({
+                    color: this._getRoadColor(highwayType),
+                    linewidth: this._getRoadWidth(highwayType),
+                    worldUnits: false,
+                    resolution: new THREE.Vector2(window.innerWidth, window.innerHeight)
+                })
+            )
             this.add(line)
         })
     }
@@ -128,5 +131,29 @@ export default class ScenePathfinding extends SceneBase {
             bbox.min.x + (dx * 0.5),
             bbox.min.y + (dy * 0.5)
         )
+    }
+
+    _getRoadWidth(highwayType: string): number {
+        switch (highwayType) {
+            case "primary": return 3.5
+            case "secondary": return 3.0
+            case "tertiary": return 2.4
+            case "residential": return 1.8
+            case "service": return 1.3
+            case "footway": return 0.8
+            case "steps": return 0.7
+        }
+        return 1
+    }
+
+    _getRoadColor(highwayType: string): THREE.Color {
+        switch (highwayType) {
+            case "primary": return new THREE.Color().setRGB(1, 1, 1)
+            case "secondary": return new THREE.Color().setRGB(0.9, 0.9, 0.9)
+            case "tertiary": return new THREE.Color().setRGB(0.8, 0.8, 0.8)
+            case "residential": return new THREE.Color().setRGB(0.7, 0.7, 0.7)
+            case "service": return new THREE.Color().setRGB(0.6, 0.6, 0.6)
+        }
+        return new THREE.Color().setRGB(1, 0.8, 0.6)
     }
 }
