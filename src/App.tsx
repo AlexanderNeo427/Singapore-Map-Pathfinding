92c27ff60e31e59e367957e0eb62be7fc8ca4438
Alexander Neo <AlexanderNeo427@gmail.com>
2025-03-02 18:04:32 +0800

    Trying to convert to using a system where the pathfinding algorithm "frames" are precomputed before being rendered conditionally. Also - added ".env" to the gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,123 +1,102 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import DeckGL from '@deck.gl/react'
-import { PickingInfo, Position as DeckPosition } from 'deck.gl'
+import { PickingInfo, Position as DeckPosition, ScatterplotLayer } from 'deck.gl'
 import { LineLayer } from 'deck.gl'
-import { Feature, FeatureCollection, LineString, Position as GeoPosition } from 'geojson';
+import { APIProvider, Map, useMap } from '@vis.gl/react-google-maps';
+import { GoogleMapsOverlay, GoogleMapsOverlayProps } from '@deck.gl/google-maps';
+import { FeatureCollection, Position as GeoPosition } from 'geojson';
 import sg_geodata from './assets/sg_geodata.json'
-import { GraphNode, Utils } from './graph_algorithms/Declarations';
+import { BfsFrameData, breadthFirstSearch, createGraphData, GraphData } from './graph_algorithms/Declarations';
 
 type FromToPair = { from: DeckPosition, to: DeckPosition }
 
 const App: React.FC = () => {
-   const m_allGraphNodesRef = useRef<Map<number, GraphNode>>(new Map())
-   const m_adjacencyListRef = useRef<Map<number, Set<number>>>(new Map())
-   const m_queueOfIDs = useRef<number[]>([])
-   const m_visitedRef = useRef<Set<number>>(new Set())
-   // const m_nodeRef = useRef<GraphNode>(new GraphNode(0, [0, 0])) // Dummy node
-   const [m_paths, setPaths] = useState<FromToPair[]>([])
-
-   const createGraphData = (alLFeatures: FeatureCollection)
-      : [Map<number, GraphNode>, Map<number, Set<number>>] => {
-
-      const allGraphNodes = new Map<number, GraphNode>()
-      const adjacencyList = new Map<number, Set<number>>()
-
-      alLFeatures.features.forEach((feature: Feature) => {
-         if (feature.geometry.type !== 'LineString') {
-            return
-         }
-         const allWayCoords: GeoPosition[] = (feature.geometry as LineString).coordinates
-         for (let i = 0; i < (allWayCoords.length - 1); i++) {
-
-            // Lazily instantiate new graph node for 'currNodeID'
-            // (assuming it doesn't exist yet)
-            const currNodeID: number = Utils.getCoordHash(allWayCoords[i])
-            if (!allGraphNodes.has(currNodeID)) {
-               const newCoords: DeckPosition = Utils.toDeckPosition(allWayCoords[i])
-               allGraphNodes.set(
-                  currNodeID, new GraphNode(currNodeID, newCoords)
-               )
-
-               // Init 'neighbour IDs' set for 'currNodeID'
-               adjacencyList.set(currNodeID, new Set())
-            }
-
-            // Lazily instantiate new graph node for 'otherNodeID' 
-            // (assuming it doesn't exist yet)
-            const otherNodeID: number = Utils.getCoordHash(allWayCoords[i + 1])
-            if (!allGraphNodes.has(otherNodeID)) {
-               const otherCoords: DeckPosition = Utils.toDeckPosition(allWayCoords[i + 1])
-               allGraphNodes.set(
-                  otherNodeID, new GraphNode(otherNodeID, otherCoords)
-               )
-
-               // Init 'neighbour IDs' set for 'otherNodeID'
-               adjacencyList.set(otherNodeID, new Set())
-            }
-
-            // Bi-directional ways
-            adjacencyList.get(currNodeID)?.add(otherNodeID)
-            adjacencyList.get(otherNodeID)?.add(currNodeID)
-         }
-      })
-      return [allGraphNodes, adjacencyList]
-   }
+   const m_map = useMap()
+   const m_overlay = useMemo(() => new GoogleMapsOverlay({} as GoogleMapsOverlayProps), [])
 
    useEffect(() => {
-      const graphData = createGraphData(sg_geodata as FeatureCollection)
-      m_allGraphNodesRef.current = graphData[0]
-      m_adjacencyListRef.current = graphData[1]
+      m_overlay.setMap(m_map)
+      return () => m_overlay.setMap(null)
+   }, [m_map, m_overlay])
 
-      const [[firstNodeID, _]] = m_allGraphNodesRef.current.entries()
-      m_queueOfIDs.current = [firstNodeID]
+   const m_graphData = useRef<GraphData>(new GraphData())
+   const [m_paths, setPaths] = useState<FromToPair[]>([])
 
-      let prevID: number | null = null;
+   useEffect(() => {
+      console.log("Stuffs: ", import.meta.env.VITE_MAPS_API_KEY)
 
-      const breadthFirstSearch = (): void => {
-         if (!m_queueOfIDs.current || m_queueOfIDs.current.length === 0) {
-            console.log("No neighbours left, exiting...")
-            return
-         }
-         const idToProcess = m_queueOfIDs.current.shift() as number
-         m_visitedRef.current.add(idToProcess) // Mark current node as visited
+      m_graphData.current = createGraphData(sg_geodata as FeatureCollection)
+      const allFrames: BfsFrameData[] = breadthFirstSearch(m_graphData.current)
 
-         // Get neighbours
-         const neighbourIDs = m_adjacencyListRef.current.get(idToProcess) as Set<number>
-         if (neighbourIDs.size === 0) {
-            console.log("No neighbours")
-            return
-         }
-
-         // Add all (unvisited) neighbours to queue
-         neighbourIDs.forEach(id => {
-            if (!m_visitedRef.current.has(id)) {
-               m_queueOfIDs.current.push(id)
-            }
-         })
-
-         const currNode = m_allGraphNodesRef.current.get(idToProcess)
-         const prevNode = prevID ? m_allGraphNodesRef.current.get(prevID) : null
+      const loop = () => {
          setPaths(oldPaths => {
-            return [...oldPaths, {
-               from: prevNode?.position || currNode?.position as DeckPosition,
-               to: currNode?.position as DeckPosition
-            }]
+            const frameData = allFrames.shift() as BfsFrameData
+            const fromPos = m_graphData.current.allGraphNodes.get(frameData.fromID)?.position as DeckPosition
+            const toPos = m_graphData.current.allGraphNodes.get(frameData.toID)?.position as DeckPosition
+            return [...oldPaths, { from: fromPos, to: toPos }]
          })
-         
-         prevID = idToProcess
-         setTimeout(breadthFirstSearch, 100)
+         setTimeout(loop, 1000)
       }
-      breadthFirstSearch()
+      loop()
    }, [])
 
-   const layer = new LineLayer<FromToPair>({
-      id: 'LineLayer',
+   const lineLayer = new LineLayer<FromToPair>({
+      id: 'Line Layer',
       data: m_paths,
       getSourcePosition: d => d.from,
       getTargetPosition: d => d.to,
-      getWidth: 12,
-      pickable: true
+      getWidth: 5,
+      pickable: true,
+      getColor: _ => [200, 100, 150]
    });
+
+   const scatterLayer = new ScatterplotLayer<FromToPair>({
+      id: 'Scatterplot Layer',
+      data: m_paths,
+      getPosition: d => d.to,
+      getRadius: 8,
+      getFillColor: _ => [150, 100, 30]
+   })
+
+   // const tripsLayer = new TripsLayer<FromPair[]>({
+   //    id: 'Trips Layer',
+   //    data: m_paths,
+   //    getPath: d => {
+   //       return d.map(p => p.from)
+   //    },
+   //    getTimestamps: d => {
+   //       return d.map(_ => 999 - 1554772579000)
+   //    },
+   //    getColor: [253, 128, 93],
+   //    currentTime: 500,
+   //    trailLength: 600,
+   //    capRounded: true,
+   //    jointRounded: true,
+   //    widthMinPixels: 8
+   // })
+
+   // type DataType = {
+   //    waypoints: {
+   //       coordinates: [longitude: number, latitude: number];
+   //       timestamp: number;
+   //    }[]
+   // };
+   //
+   // const tripsLayer = new TripsLayer<DataType>({
+   //    id: 'TripsLayer',
+   //    data: 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf.trips.json',
+   //    getPath: (d: DataType) => {
+   //       console.log(d)
+   //       return d.waypoints.map(p => p.coordinates)
+   //    },
+   //    getTimestamps: (d: DataType) => d.waypoints.map(p => p.timestamp - 1554772579000),
+   //    getColor: [253, 128, 93],
+   //    currentTime: 500,
+   //    trailLength: 600,
+   //    capRounded: true,
+   //    jointRounded: true,
+   //    widthMinPixels: 8
+   // });
 
    return (
       <div style={{
@@ -126,14 +105,16 @@ const App: React.FC = () => {
          alignItems: 'center',
          width: '100%', height: '80%',
       }}>
-         <DeckGL
-            initialViewState={{
-               longitude: 103.873, latitude: 1.348, zoom: 15,
-            }}
-            controller
-            layers={[layer]}
-            onClick={(info: PickingInfo) => console.log("Clicked: ", info.coordinate)}
-         />
+         <APIProvider apiKey={`${import.meta.env.GOOGLE_MAPS_API_KEY}`}>
+            <DeckGL
+               initialViewState={{
+                  longitude: 103.8639095677252, latitude: 1.3538345746338294, zoom: 17,
+               }}
+               controller
+               layers={[lineLayer]}
+               onClick={(info: PickingInfo) => console.log("Clicked: ", info.coordinate)}
+            />
+         </APIProvider>
       </div>
    )
 }
--- src/graph_algorithms/Declarations.ts
+++ src/graph_algorithms/Declarations.ts
@@ -1,5 +1,5 @@
-import { Position as DeckPosition } from "deck.gl"
-import { Position as GeoPosition } from "geojson";
+import { Deck, Position as DeckPosition } from "deck.gl"
+import { Feature, FeatureCollection, Position as GeoPosition, LineString } from "geojson";
 
 export const Utils = {
     hashCode(s: string): number {
@@ -12,14 +12,9 @@ export const Utils = {
     getCoordHash(geoCoord: GeoPosition): number {
         return this.hashCode(`${geoCoord[0]}, ${geoCoord[1]}`)
     },
-    toDeckPosition(geoPos: GeoPosition): DeckPosition {
-        return [geoPos[0], geoPos[1]]
-    },
-    toGeoPosition(deckPos: DeckPosition): DeckPosition {
-        return [deckPos[0], deckPos[1]]
-    },
+    toDeckPosition(geoPos: GeoPosition): DeckPosition { return [geoPos[0], geoPos[1]] },
+    toGeoPosition(deckPos: DeckPosition): DeckPosition { return [deckPos[0], deckPos[1]] },
 }
-
 
 export class GraphNode {
     public readonly ID: number
@@ -31,3 +26,87 @@ export class GraphNode {
     }
 }
 
+export class BfsFrameData {
+    public readonly fromID: number
+    public readonly toID: number
+
+    constructor(fromID: number, toID: number) {
+        this.fromID = fromID
+        this.toID = toID
+    }
+}
+
+export class GraphData {
+    public allGraphNodes: Map<number, GraphNode> = new Map()
+    public adjacencyList: Map<number, Set<number>> = new Map()
+}
+
+export const createGraphData = (alLFeatures: FeatureCollection): GraphData => {
+    const allGraphNodes = new Map<number, GraphNode>()
+    const adjacencyList = new Map<number, Set<number>>()
+
+    alLFeatures.features.forEach((feature: Feature) => {
+        if (feature.geometry.type !== 'LineString') { return }
+
+        const allWayCoords: GeoPosition[] = (feature.geometry as LineString).coordinates
+        for (let i = 0; i < (allWayCoords.length - 1); i++) {
+
+            // Lazily instantiate new graph node for 'currNodeID'
+            // (assuming it doesn't exist yet)
+            const currNodeID: number = Utils.getCoordHash(allWayCoords[i])
+            if (!allGraphNodes.has(currNodeID)) {
+                const newCoords: DeckPosition = Utils.toDeckPosition(allWayCoords[i])
+                allGraphNodes.set(currNodeID, new GraphNode(currNodeID, newCoords))
+
+                // Init 'neighbour IDs' set for 'currNodeID'
+                adjacencyList.set(currNodeID, new Set())
+            }
+
+            // Lazily instantiate new graph node for 'otherNodeID' 
+            // (assuming it doesn't exist yet)
+            const otherNodeID: number = Utils.getCoordHash(allWayCoords[i + 1])
+            if (!allGraphNodes.has(otherNodeID)) {
+                const otherCoords: DeckPosition = Utils.toDeckPosition(allWayCoords[i + 1])
+                allGraphNodes.set(otherNodeID, new GraphNode(otherNodeID, otherCoords))
+
+                // Init 'neighbour IDs' set for 'otherNodeID'
+                adjacencyList.set(otherNodeID, new Set())
+            }
+
+            // Bi-directional ways
+            adjacencyList.get(currNodeID)?.add(otherNodeID)
+            adjacencyList.get(otherNodeID)?.add(currNodeID)
+        }
+    })
+    return { allGraphNodes: allGraphNodes, adjacencyList: adjacencyList }
+}
+
+export const breadthFirstSearch = (graphData: GraphData): BfsFrameData[] => {
+    if (graphData.allGraphNodes.size === 0) {
+        return []
+    }
+    const visitedIDs = new Set<number>()
+    const queueOfIDs = new Array<number>()
+    const [[firstNodeID, _]] = graphData.adjacencyList.entries()
+    queueOfIDs.push(firstNodeID)
+    let prevIdToProcess: number = firstNodeID
+
+    const allFrameData: BfsFrameData[] = []
+    while (queueOfIDs.length > 0) {
+        const idToProcess = queueOfIDs.shift() as number
+        visitedIDs.add(idToProcess)
+
+        const neighbourIDs = graphData.adjacencyList.get(idToProcess) as Set<number>
+        neighbourIDs.forEach((idOfNeighbour: number) => {
+            if (!visitedIDs.has(idOfNeighbour)) {
+                queueOfIDs.push(idOfNeighbour)
+            }
+        })
+
+        if (idToProcess !== prevIdToProcess) {
+            allFrameData.push(new BfsFrameData(prevIdToProcess, idToProcess))
+            prevIdToProcess = idToProcess
+        }
+    }
+    return allFrameData
+}
