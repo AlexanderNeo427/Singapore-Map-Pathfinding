2dd3e564e4777c9661dc42c9697067efa8022551
Alexander Neo <AlexanderNeo427@gmail.com>
2025-05-21 01:02:34 +0800

    Managed to find the VERY subtle bug with serialization.
    That was annoying. For some reason, naming a property within a protobuf message as "from_id" doesn't allow it to serialize properly - or rather, it seemed to create protobuf object properly, but for some reason it wouldn't deserialize properly

--- src/typescript/preprocessor/preprocessor.ts
+++ src/typescript/preprocessor/preprocessor.ts
@@ -1,69 +1,77 @@
 import singaporeRoadData from '../../assets/roadData.json'
 import { FeatureCollection } from 'geojson'
-import { GraphNode } from '../Declarations'
 import GraphHelpers from '../GraphHelpers'
 import protobuf from 'protobufjs'
 import path from 'path'
 import fs from 'fs'
 
+const PROTO_NAME = "pathfinding.proto"
+const NODE_TYPE = "Pathfinding.Node"
+const EDGE_TYPE = "Pathfinding.Edge"
+const GRAPH_TYPE = "Pathfinding.Graph"
+const OUTPUT_NAME = "graph_data.bin"
+
 const preprocess = async (geoJson: FeatureCollection): Promise<void> => {
     const graphData = GraphHelpers.buildGraph(geoJson)
-    const protoPath = path.resolve(__dirname, 'pathfinding.proto');
+    const protoPath = path.resolve(__dirname, PROTO_NAME)
 
     const root = await protobuf.load(protoPath)
-    const nodeType = root.lookupType('Pathfinding.Node')
-    // const edgeType = root.lookupType('pathfinding.Edge')
-    const graphType = root.lookupType('Pathfinding.Graph')
+    const nodeType = root.lookupType(NODE_TYPE)
+    const edgeType = root.lookupType(EDGE_TYPE)
+    const graphType = root.lookupType(GRAPH_TYPE)
 
-    const obj = nodeType.verify({ id: 59 })
-    console.log(obj)
-    const nodesPb = Array.from(graphData.allGraphNodes).map(([id, node]) => {
+    const pbNodes = Array.from(graphData.allGraphNodes).map(([id, node]) => {
         const payload = {
             id: id.toString(),
             x: node.position[0].toString(),
             y: node.position[1].toString(),
         }
-        const err = nodeType.verify({ asd: "asd " })
-        if (err) {
-            throw new Error(err)
-        }
         return nodeType.create(payload)
     })
 
-    // const edgesPb = Array.from(graphData.adjacencyList).map(([id, idOfNeighbours]) => {
-    //     const data: protobuf.Message<{}>[] = []
+    const pbEdges = (
+        (): protobuf.Message[] => {
+            const arr: protobuf.Message[] = []
+            graphData.adjacencyList.forEach((allNeighbourIDs, nodeID) => {
+                allNeighbourIDs.forEach(neighbourID => {
+                    const payload = {
+                        fromID: nodeID.toString(),
+                        toID: neighbourID.toString()
+                    }
+                    const edge = edgeType.create(payload)
+                    arr.push(edge)
+                })
+            })
+            return arr
+        }
+    )()
 
-    //     idOfNeighbours.forEach(neighbourID => {
-    //         const firstPayload = {
-    //             from_id: id,
-    //             to_id: neighbourID
-    //         }
-    //         const secondPayload = {
-    //             from_id: neighbourID,
-    //             to_id: id
-    //         }
-    //     })
+    const pbGraph = graphType.create({
+        nodes: pbNodes,
+        edges: pbEdges
+    })
 
-    //     return data
-    // })
+    const binaryData = graphType.encode(pbGraph).finish()
+    const outputPath = path.resolve(__dirname, OUTPUT_NAME)
+    fs.writeFileSync(outputPath, binaryData)
 
-    // console.log(
-    //     "Is Graph Verified: ",
-    //     graphType.verify({ nodes: nodesPb })
-    // )
-    // const nodeBuffers = await getNodeBuffers(
-    //     graphData.allGraphNodes,
-    //     root.lookupType('Graph.Node')
-    // )
-    // const edgeBuffers = await getEdgeBuffers(
-    //     graphData.adjacencyList,
-    //     root.lookupType('Graph.Edge')
-    // )
-    // fs.writeFileSync(path.resolve(__dirname, ""), nodeBuffers)
+    console.log("Successfully written data to: ", outputPath)
 }
 
+// const deserialize = async (): Promise<void> => {
+//     const protoPath = path.resolve(__dirname, PROTO_NAME)
+//     const root = await protobuf.load(protoPath)
+//     const graphType = root.lookupType(GRAPH_TYPE)
+
+//     const inputPath = path.resolve(__dirname, OUTPUT_NAME)
+//     const binaryData = fs.readFileSync(inputPath)
+
+//     const deserializedGraph = graphType.decode(binaryData)
+//     console.log("Deserialized Graph: ", deserializedGraph)
+// }
+
 try {
-    preprocess(singaporeRoadData as FeatureCollection)
+    await preprocess(singaporeRoadData as FeatureCollection)
 } catch (err) {
     console.error((err as Error).message)
 }
